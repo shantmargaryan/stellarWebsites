83c4019c9b2a6a4a7f113ca014a523fd2ce01a1c
Shant <shant212205@gmail.com>
2024-04-19 05:56:45 -0800

    update

---
+++ js/showMore.js
@@ -0,0 +1,28 @@
+function showMore(showItems, itemVisibleLength, showMore) {
+    const items = document.querySelectorAll(showItems);
+    const button = document.querySelector("[data-show-more]");
+    const itemsLength = items.length;
+
+    let currentItemsLength = showMore;
+
+    button.addEventListener("click", () => {
+        currentItemsLength += showMore;
+        const itemsArray = Array.from(items);
+        const showMoreItems = itemsArray.slice(3, currentItemsLength);
+
+
+        showMoreItems.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        if (currentItemsLength >= itemsLength) {
+            button.style.display = 'none';
+        }
+    });
+
+    for (let i = 0; i < itemVisibleLength; i++) {
+        items[i].style.display = 'block';
+    }
+};
+
+showMore("[data-show-item]", 3, 4);
